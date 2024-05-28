d4bfa26ef2f74bb998710a45ad7dd5c0637f5eb8
Charishma974 <cherryspl09@gmail.com>
2024-05-28 09:18:14 +0530

    Added Title using Helmet

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Header from './Header'
+import Title from '../shared/Title'
 
 const AppLayout = () => (WrappedComponent) => {
   return (props) => {
     return (
-        <div>
-            <Header />
-            <WrappedComponent {...props} />
-            <div>Footer</div>
-        </div>
+      <>
+        <Title />
+        <Header />
+        <WrappedComponent {...props} />
+      </>
     )
   }
 }
