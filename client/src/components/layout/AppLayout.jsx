124dea1cb858a5d0ccc09fa765d4c7ec768bf958
Charishma974 <cherryspl09@gmail.com>
2024-05-29 23:33:55 +0530

    Updated ChatList.jsx and ChatItem.jsx file

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -19,7 +19,7 @@ const AppLayout = () => (WrappedComponent) => {
 
         <Grid container height={"calc(100vh - 4rem)"}>
           <Grid item sm={4} md={3} sx={{ display: { xs: "none", sm: "block" }, }} height="100%">
-            <ChatList chats={sampleChats} />
+            <ChatList chats={sampleChats} chatId={chatId} newMessagesAlert={[{chatId,count: 4}]} onlineUsers={["1","2"]} />
           </Grid>
           <Grid item xs={12} sm={8} md={5} lg={6} height="100%"><WrappedComponent {...props} /></Grid>
           <Grid item md={4} lg={3} sx={{ display: { xs: "none", md: "block" }, padding: "2rem", bgcolor: "rgba(0,0,0,0.85)" }} height="100%">Third</Grid>
