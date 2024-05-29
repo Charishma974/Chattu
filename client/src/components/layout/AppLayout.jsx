adc3bb34e60715f974c87eec9af606a707018aae
Charishma974 <cherryspl09@gmail.com>
2024-05-29 19:54:13 +0530

    Added Main Loader and Dialog Loaders Successfully.

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Title from '../shared/Title'
 import { Grid } from '@mui/material'
+import ChatList from "../specific/ChatList.jsx";
 
 const AppLayout = () => (WrappedComponent) => {
   return (props) => {
@@ -11,7 +12,7 @@ const AppLayout = () => (WrappedComponent) => {
         <Header />
 
         <Grid container height={"calc(100vh - 4rem)"}>
-          <Grid item sm={4} md={3} sx={{display: {xs: "none",sm: "block"},}} height="100%">First</Grid>
+          <Grid item sm={4} md={3} sx={{display: {xs: "none",sm: "block"},}} height="100%"><ChatList /></Grid>
           <Grid item xs={12} sm={8} md={5} lg={6} height="100%"><WrappedComponent {...props} /></Grid>
           <Grid item md={4} lg={3} sx={{display: {xs: "none",md: "block"},padding: "2rem",bgcolor: "rgba(0,0,0,0.85)"}} height="100%">Third</Grid>
         </Grid>
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,33 +1,36 @@
-import React, { useState } from 'react'
+import React, { Suspense, lazy, useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { orange } from "../../constants/color.js";
-import { AppBar, Box, IconButton, Toolbar, Tooltip, Typography } from '@mui/material';
-import { Menu as MenuIcon, Search as SearchIcon, Add as AddIcon, Group as GroupIcon,Logout as LogoutIcon, Notifications as NotificationsIcon } from "@mui/icons-material"
+import { AppBar, Backdrop, Box, IconButton, Toolbar, Tooltip, Typography } from '@mui/material';
+import { Menu as MenuIcon, Search as SearchIcon, Add as AddIcon, Group as GroupIcon, Logout as LogoutIcon, Notifications as NotificationsIcon } from "@mui/icons-material"
+
+const SearchDialog = lazy(()=>import("../specific/Search.jsx"))
+const NotificationDialog = lazy(()=>import("../specific/Notifications.jsx"));
+const NewGroupDialog = lazy(()=>import("../specific/NewGroup.jsx"));
 
 const Header = () => {
 
   const navigate = useNavigate();
 
-  const [isMobile,setIsMobile] = useState(false);
-  const [isSearch,setIsSearch] = useState(false);
-  const [isNewGroup,setIsNewGroup] = useState(false);
-  const [isNotification,setIsNotification] = useState(false);
-
+  const [isMobile, setIsMobile] = useState(false);
+  const [isSearch, setIsSearch] = useState(false);
+  const [isNewGroup, setIsNewGroup] = useState(false);
+  const [isNotification, setIsNotification] = useState(false);
 
   const handleMobile = () => {
-    setIsMobile(prev=>!prev);
+    setIsMobile(prev => !prev);
   }
 
   const openSearch = () => {
-    setIsSearch(prev=>!prev);
+    setIsSearch(prev => !prev);
   }
 
   const openNewGroup = () => {
-    setIsNewGroup(prev=>!prev);
+    setIsNewGroup(prev => !prev);
   }
 
   const openNotification = () => {
-    setIsNotification(prev=>!prev);
+    setIsNotification(prev => !prev);
   }
 
   const navigateToGroup = () => {
@@ -39,24 +42,29 @@ const Header = () => {
   }
 
   return (
-    <Box sx={{ flexGrow: 1 }} height="4rem">
-      <AppBar position="static" sx={{ bgcolor: orange }}>
-        <Toolbar>
-          <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>Chattu</Typography>
-          <Box sx={{ display: { xs: "block", sm: "none" } }}>
-            <IconButton color="inherit" onClick={handleMobile}><MenuIcon /></IconButton>
-          </Box>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box>
-            <IconBtn title={"Search"} icon={<SearchIcon />} onClick={openSearch} />
-            <IconBtn title={"New Group"} icon={<AddIcon />} onClick={openNewGroup} />
-            <IconBtn title={"Manage Groups"} icon={<GroupIcon />} onClick={navigateToGroup} />
-            <IconBtn title={"Notifications"} icon={<NotificationsIcon />} onClick={openNotification} />
-            <IconBtn title={"Logout"} icon={<LogoutIcon />} onClick={logoutHandler} />
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }} height="4rem">
+        <AppBar position="static" sx={{ bgcolor: orange }}>
+          <Toolbar>
+            <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>Chattu</Typography>
+            <Box sx={{ display: { xs: "block", sm: "none" } }}>
+              <IconButton color="inherit" onClick={handleMobile}><MenuIcon /></IconButton>
+            </Box>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box>
+              <IconBtn title={"Search"} icon={<SearchIcon />} onClick={openSearch} />
+              <IconBtn title={"New Group"} icon={<AddIcon />} onClick={openNewGroup} />
+              <IconBtn title={"Manage Groups"} icon={<GroupIcon />} onClick={navigateToGroup} />
+              <IconBtn title={"Notifications"} icon={<NotificationsIcon />} onClick={openNotification} />
+              <IconBtn title={"Logout"} icon={<LogoutIcon />} onClick={logoutHandler} />
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {isSearch && (<Suspense fallback={<Backdrop open />}><SearchDialog /></Suspense>)}
+      {isNotification && (<Suspense fallback={<Backdrop open />}><NotificationDialog /></Suspense>)}
+      {isNewGroup && (<Suspense fallback={<Backdrop open />}><NewGroupDialog /></Suspense>)}
+    </>
   )
 }
 
