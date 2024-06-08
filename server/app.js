a71fac1bd6b343c83ee5d1f2acb9a4c2539bcf78
Charishma974 <cherryspl09@gmail.com>
2024-06-08 17:24:20 +0530

    Added Login and Register Routes with ErrorHandling Successfully.

--- server/app.js
+++ server/app.js
@@ -1,14 +1,29 @@
+import dotenv from "dotenv";
 import express from "express";
 import userRoute from "./routes/user.js";
+import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
+
+dotenv.config({path: "./.env"});
+
+const mongoURI = process.env.MONGO_URI
+const port = process.env.PORT || 3000
+
+connectDB(mongoURI);
 
 const app = express();
+
+// Using Middleware Here
+app.use(express.json());
 
 app.use("/user",userRoute);
 
 app.get("/",(req,res)=>{
-    res.send("Hello Charishma... New to Node JS");
+    res.send("Hello World");
 })
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.use(errorMiddleware);
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
 })
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,47 @@
-const login = (req,res) => {
-    res.send("Hello World!!!");
+import { TryCatch } from "../middlewares/error.js";
+import {User} from "../models/user.js";
+import {sendToken} from "../utils/features.js";
+import {compare} from "bcrypt";
+import { ErrorHandler } from "../utils/utility.js";
+
+// Create a new user and save it to the database and save token in cookie
+const newUser = async (req, res) => {
+
+    const {name,username,password,bio} = req.body;
+
+    const avatar = {
+        public_id: "Sdfsd",
+        url: "asdfd"
+    }
+
+    const user = await User.create({
+        name,
+        bio,
+        username,
+        password,
+        avatar
+    })
+
+    sendToken(res,user,201,"User created");
 }
 
-export {login};
+// Login user and save token in cookie
+const login = TryCatch(async (req, res,next) => {
+
+    const {username,password} = req.body;
+
+    const user = await User.findOne({username}).select("+password");
+
+    if (!user) return next(new ErrorHandler("Invalid Username or Password",404));
+
+    const isMatch = await compare(password,user.password);
+
+    if (!isMatch) return next(new ErrorHandler("Invalid Username or Password",404));
+
+    sendToken(res,user,200,`Welcome Back, ${user.name}`);
+
+})
+
+const getMyProfile = async (req,res) => {}
+
+export { login, newUser,getMyProfile };
