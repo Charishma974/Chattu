1226dba893fa3037aff7908e93627ac2588dc5dc
Charishma974 <cherryspl09@gmail.com>
2024-06-08 11:39:31 +0530

    Created Basic Mongoose Models Successfully.

--- server/app.js
+++ server/app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use("/user",userRoute);
 
+app.get("/",(req,res)=>{
+    res.send("Hello Charishma... New to Node JS");
+})
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
