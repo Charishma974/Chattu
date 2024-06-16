3597d7824609eedc4bc38fa3945ff2c8b67bdc20
Charishma974 <cherryspl09@gmail.com>
2024-06-17 01:49:06 +0530

    Created Group Chat API Successfully.

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -58,4 +58,13 @@ const logout = TryCatch(async (req,res) => {
     })
 })
 
-export { login, newUser,getMyProfile,logout };
+const searchUser = TryCatch(async(req,res)=>{
+    const {name} = req.query;
+
+    return res.status(200).json({
+        success: true,
+        message: name,
+    })
+})
+
+export { login, newUser,getMyProfile,logout,searchUser };
