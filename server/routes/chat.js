04eaafdb1c718248fcb8e1e58c841163ff612ab0
Charishma974 <cherryspl09@gmail.com>
2024-06-18 09:34:51 +0530

    Created API for getting my groups list.

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { getMyChats, newGroupChat } from "../controllers/chat.js";
+import { getMyChats, getMyGroups, newGroupChat } from "../controllers/chat.js";
 
 const app = express.Router();
 
@@ -11,5 +11,7 @@ app.use(isAuthenticated);
 app.post("/new",newGroupChat);
 
 app.post("/my",getMyChats);
+
+app.get("/my/groups",getMyGroups);
 
 export default app;
