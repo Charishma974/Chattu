ffed14198a4ab701a5da2c8dafbe33d8ed4fd69f
Charishma974 <cherryspl09@gmail.com>
2024-06-18 11:32:43 +0530

    Sending Attachments

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { addMembers, getMyChats, getMyGroups, leaveGroup, newGroupChat, removeMember } from "../controllers/chat.js";
+import { addMembers, getMyChats, getMyGroups, leaveGroup, newGroupChat, removeMember, sendAttachments } from "../controllers/chat.js";
+import { attachmentsMulter } from "../middlewares/multer.js";
 
 const app = express.Router();
 
@@ -21,9 +22,11 @@ app.put("/removemember",removeMember);
 app.delete("/leave/:id",leaveGroup);
 
 // Send Attachments
+app.post("/message",attachmentsMulter,sendAttachments);
 
 // Get Messages
 
 // Get Chat Details, rename, delete
+app.route("/:id").get().put().delete();
 
 export default app;
